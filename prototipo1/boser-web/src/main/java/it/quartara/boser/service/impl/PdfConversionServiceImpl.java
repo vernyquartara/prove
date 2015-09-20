dc7088dcb6ebaeab01664ec209a53d482e75bfc0
verny <webny23@gmail.com>
2015-09-20 19:18:08 +0200

    impostate nuove dimensioni pagina 190x900 a scopo di test. avanzata versione cssbox a 4.9

--- prototipo1/boser-web/src/main/java/it/quartara/boser/service/impl/PdfConversionServiceImpl.java
+++ prototipo1/boser-web/src/main/java/it/quartara/boser/service/impl/PdfConversionServiceImpl.java
@@ -3,12 +3,10 @@ package it.quartara.boser.service.impl;
 import it.quartara.boser.helper.UrlHelper;
 import it.quartara.boser.service.PdfConversionService;
 
-import java.awt.Color;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
@@ -22,15 +20,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.PageSize;
 import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.RectangleReadOnly;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfImportedPage;
 import com.lowagie.text.pdf.PdfReader;
-import com.lowagie.text.pdf.PdfStamper;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
@@ -101,14 +95,17 @@ public class PdfConversionServiceImpl implements PdfConversionService, Serializa
 			PdfReader reader = new PdfReader(new ByteArrayInputStream(intermediatePdf.toByteArray()));
 			Rectangle originalRectangle = reader.getPageSize(1);
 			
-			float wOut = PageSize.A4.getWidth();
-			float hOut = PageSize.A4.getHeight();
+//			float wOut = PageSize.A4.getWidth();
+//			float hOut = PageSize.A4.getHeight();
+			float wOut = 538.33f;
+			float hOut = 2551.51f;
 			
 			float scale = wOut / originalRectangle.getWidth(); //rapporto la larghezza A4 e la larghezza originale
 			float scaledHeigth = originalRectangle.getHeight() * scale; //altezza originale scalata
 			int numPages = (int) Math.ceil(scaledHeigth / hOut); //numero di pagine calcolato in base all'altezza scalata
 			
-		    Document doc = new Document(PageSize.A4);
+		    //Document doc = new Document(PageSize.A4);
+		    Document doc = new Document(new RectangleReadOnly(wOut,hOut));
 		    PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(destFile));
 		    doc.open();
 		    PdfContentByte content = writer.getDirectContent();
@@ -149,67 +146,4 @@ public class PdfConversionServiceImpl implements PdfConversionService, Serializa
 		return destFile;
 	}
 	
-	public static void main(String ... args) {
-		PdfConversionServiceImpl service = new PdfConversionServiceImpl();
-		File pdfFile = service.convertToPdf("/home/webny/work/Boser/test/pdf", 
-				"http://www.gazzetta.it/Passione-Motori/Auto/10-04-2015/nuova-kia-rio-clima-navigatore-telecamera-motori-110408679133.shtml", 
-				"gazzetta.it");
-	}
-	
-	/*
-	 * for debug purpose
-	 */
-	public static void mainssss(String[] a) throws IOException, DocumentException {
-		PdfConversionServiceImpl service = new PdfConversionServiceImpl();
-		File pdfFile = service.convertToPdf("/home/webny/work/Boser/test/pdf", "http://www.riders.org/get-involved/motorcycling-events", "riders.org");
-		PdfReader reader = new PdfReader(pdfFile.getAbsolutePath());
-		PdfStamper pdfStamper = new PdfStamper(reader,
-	            new FileOutputStream("/home/webny/work/Boser/test/pdf/HelloWorld-Stamped.pdf"));
-		for(int i=1; i<= reader.getNumberOfPages(); i++){
-
-	          PdfContentByte content = pdfStamper.getUnderContent(i);
-	          /*
-	          PdfPTable pdffooter = new PdfPTable(1);
-              pdffooter.setTotalWidth(550); 
-              pdffooter.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-              pdffooter.getDefaultCell().setVerticalAlignment(Element.ALIGN_BOTTOM); 
-              pdffooter.getDefaultCell().setBorderColor(new Color(255, 255, 255));
-              pdffooter.addCell(new Phrase("---------http://www.riders.org/get-involved/motorcycling-events--------", new Font(Font.HELVETICA, 8, Font.ITALIC, new Color(0, 0, 0)) ));
-              pdffooter.writeSelectedRows(0, -1, 0, content.getPdfDocument().bottom(), content);
-	          */
-
-	          content.beginText();
-	          content.setFontAndSize(BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.EMBEDDED), 18);
-	          content.setTextMatrix(30, 30);
-	          content.showText("---------http://www.riders.org/get-involved/motorcycling-events--------");
-	          content.setFontAndSize(BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.EMBEDDED), 32);
-	          content.setColorStroke(Color.BLACK);
-	          content.showTextAligned(Element.ALIGN_CENTER, "DUPLICATE", 230, 1000, 45);
-	          content.endText();
-	          
-	          /*
-	          content.beginText();
-	          content.setFontAndSize(BaseFont.createFont(BaseFont.HELVETICA, "UTF-8", false),12);
-	          String text = "---------http://www.riders.org/get-involved/motorcycling-events--------";
-	          content.showTextAligned(PdfContentByte.ALIGN_LEFT,text,300,40, 0);
-	          content.endText();
-	          */
-	      }
-		pdfStamper.close();
-		
-		/*
-		Document document = new Document(reader.getPageSizeWithRotation(1));
-		//reader.get
-		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("/home/webny/work/Boser/test/pdf/HelloWorld.pdf"));
-		HeaderFooter footer = new HeaderFooter(new Phrase("Add Footer Here"), new Phrase("."));
-		footer.setBorder(Rectangle.NO_BORDER);
-        footer.setAlignment(Element.ALIGN_CENTER);
-		writer.setPageEvent(footer);
-		document.open();
-		
-		document.setFooter(footer);
-		document.close();
-		*/
-	}
-
 }
